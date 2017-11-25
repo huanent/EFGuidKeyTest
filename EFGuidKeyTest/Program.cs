1b7303329f11cd15e41776028118d511566a09d3
李凤焕 <huanent@hotmail.com>
2017-11-25 19:16:01 +0800

    添加项目文件。

---
+++ EFGuidKeyTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFGuidKeyTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
